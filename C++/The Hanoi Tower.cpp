e1f14baa610755a1e1214ed62065a8fb7a35bdff
CaptainMirage <87281406+CaptainMirage@users.noreply.github.com>
2025-01-28 10:08:33 +0330

    The Hanoi Tower intitial commit

---
+++ C++/The Hanoi Tower.cpp
@@ -0,0 +1,46 @@
+/**
+ * Problem Description:
+ * -------------------
+ * The Hanoi Tower is a mathematical puzzle involving three pegs and a set of disks of different sizes.
+ * The objective is to move an entire stack of disks from one peg to another, following these strict rules:
+ * 1. Only one disk can be moved at a time
+ * 2. Each disk is moved from one peg to another
+ * 3. No larger disk can be placed on top of a smaller disk
+ * The minimum number of moves required is 2^n - 1, where n is the number of disks.
+ * 
+ * Historical Context:
+ * ------------------
+ * The problem was first introduced by French mathematician Édouard Lucas in 1883.
+ * Lucas was inspired by a fictional legend of a temple in Hanoi, Vietnam, where monks move 64 golden disks 
+ * between three pegs, following the same rules. According to the legend, when the monks complete 
+ * moving all 64 disks, the world will end. This mythical puzzle would require 2^64 - 1 moves, 
+ * which would take approximately 585 billion years to complete.
+ * 
+ * Implementation Notes:
+ * --------------------
+ * i just translated the rust version, dont judge me im lazy currently
+ */
+#include <iostream>
+using namespace std;
+
+class solution {
+    public:
+    static void hanoi_tower(int n, char source, char auxiliary, char destination) {
+        if (n == 1) {
+            std::cout << "Move disk 1 from {} to {}" << '\n';
+            return;
+        }
+        hanoi_tower(n - 1, source, destination, auxiliary);
+        std::cout << "Move disk 1 from {} to {}" << '\n';
+        hanoi_tower(n - 1, auxiliary, source, destination);
+    }
+};
+
+int main(int argc, char* argv[])
+{
+    int n = 12;
+    solution::hanoi_tower(n,
+                          'A',
+                          'B',
+                          'C');
+}
