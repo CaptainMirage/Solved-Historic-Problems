9c65011458c82693d1b7a8cb721a5e70f30ce71a
CaptainMirage <87281406+CaptainMirage@users.noreply.github.com>
2025-01-29 12:55:57 +0330

    Josephus Problem overhaul

--- C++/The Josephus Problem.cpp
+++ C++/The Josephus Problem.cpp
@@ -15,9 +15,9 @@ Siege of Yodfat. They chose suicide over capture and decided to form a circle an
 third person until no one was left. Josephus, not wanting to die, quickly calculated where he
 should stand to be the last survivor.
 
-Implementation Notes:
-------------------
-im too lazy to add anything else at the moment so deal with it :D
+Dev Notes:
+---------
+finally updated the code to have test cases, no comments for you tho
 */
 #include <iostream>
 using namespace std;
@@ -28,9 +28,28 @@ public:
         if (n == 1) {return 0;}
         return (josephus_prob(n - 1, k) + k) % n;
     }
+    
+    static void test_cases(int test_number, int k, int length, int expected) {
+        int actual = josephus_prob(length, k) + 1;
+        
+        printf("\nTest #%d debug :\n", test_number);
+        printf("Expected result = %d\n", expected);
+        printf("actual result = %d\n", actual);
+
+        if (actual == expected) {
+            printf("Test %d passed: k=%d, length=%d\n", test_number, k, length);
+        } else {
+            printf("Test %d failed: k=%d, length=%d\n", test_number, k, length);
+        }
+    }
 };
 int main() {
-    constexpr int n = 5;
-    constexpr int k = 2;
-    std::cout << solution::josephus_prob(n, k) + 1;
+    solution::test_cases(1, 3, 42, 34);
+    solution::test_cases(2, 1, 5, 5);
+    solution::test_cases(3, 3, 15, 5);
+    
+    constexpr int n = 41;
+    constexpr int k = 3;
+    int survivor = solution::josephus_prob(n, k) + 1;
+    printf("\nThe survivor is at position: %d", survivor);
 }
